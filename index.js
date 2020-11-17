e74d1d87f562136067bc86d657e22d6cb23e66fd
ellenupark <ellenupark@gmail.com>
2020-11-17 13:27:46 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+document.getElementById("main").remove()
+
+let newHeader = document.createElement('h1')
+newHeader.id = 'victory'
+newHeader.innerHTML = "Ellen is the champion"
+document.body.appendChild(newHeader)
