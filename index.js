f2349648a0b623bbf6b54d3cb09e7c895a5756d1
Tabatha Slatton <valbatha@gmail.com>
2020-11-17 13:11:50 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code here!
+
+// remove main from html
+let main = document.querySelector('main#main');
+main.remove()
+
+// create new header 
+let newHeader = document.createElement('h1')
+document.body.appendChild(newHeader)
+newHeader.id = 'victory'
+newHeader.innerHTML = "Tabatha is the champion"
