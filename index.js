9e8422e32d0a7546d6875e215843aad0f88ade04
Eli Cusic <tntitans31@gmail.com>
2020-11-18 09:56:23 -0600

    Done.

--- index.js
+++ index.js
@@ -2,4 +2,6 @@
 let main = document.querySelector("main#main")
 main.remove()
 
-const newHeader = document.createElement("h1#victory")
+const newHeader = document.createElement("h1")
+newHeader.id = "victory"
+newHeader.innerHTML = "I is the champion!"
